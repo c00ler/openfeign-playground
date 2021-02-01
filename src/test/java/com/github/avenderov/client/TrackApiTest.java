f53d7e6bf04d75c5db38b853ef644e2805ea4fb6
Alexey Venderov <alexey.venderov@getmoss.com>
2021-02-01 19:29:20 +0100

    Bump spring boot and feign versions

--- src/test/java/com/github/avenderov/client/TrackApiTest.java
+++ src/test/java/com/github/avenderov/client/TrackApiTest.java
@@ -11,25 +11,23 @@ import feign.jackson.JacksonDecoder;
 import feign.jackson.JacksonEncoder;
 import feign.okhttp.OkHttpClient;
 import feign.slf4j.Slf4jLogger;
-import org.junit.Before;
-import org.junit.Test;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.context.embedded.LocalServerPort;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.boot.web.server.LocalServerPort;
 
 import java.net.SocketTimeoutException;
 import java.util.Collection;
 import java.util.Collections;
 import java.util.UUID;
+import java.util.concurrent.TimeUnit;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.catchThrowable;
 
-@RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-public class TrackApiTest {
+class TrackApiTest {
 
     @LocalServerPort
     private int port;
@@ -39,30 +37,30 @@ public class TrackApiTest {
 
     private TrackApi underTest;
 
-    @Before
-    public void beforeEach() {
+    @BeforeEach
+    void beforeEach() {
         underTest =
-                Feign.builder()
-                        .logLevel(Logger.Level.FULL)
-                        .logger(new Slf4jLogger(TrackApiTest.class))
-                        .encoder(new JacksonEncoder())
-                        .decoder(new JacksonDecoder())
-                        .decode404()
-                        .client(new OkHttpClient())
-                        .options(new Request.Options(100, 500))
-                        .retryer(Retryer.NEVER_RETRY)
-                        .target(TrackApi.class, "http://localhost:" + port);
+            Feign.builder()
+                .logLevel(Logger.Level.FULL)
+                .logger(new Slf4jLogger(TrackApiTest.class))
+                .encoder(new JacksonEncoder())
+                .decoder(new JacksonDecoder())
+                .decode404()
+                .client(new OkHttpClient())
+                .options(new Request.Options(100, TimeUnit.MILLISECONDS, 500, TimeUnit.MILLISECONDS, true))
+                .retryer(Retryer.NEVER_RETRY)
+                .target(TrackApi.class, "http://localhost:" + port);
     }
 
     @Test
-    public void shouldReturnAllTracks() {
+    void shouldReturnAllTracks() {
         final Collection<Track> tracks = underTest.tracks();
 
         assertThat(tracks).hasSize(5);
     }
 
     @Test
-    public void shouldReturnOneTrack() {
+    void shouldReturnOneTrack() {
         final Track expectedTrack = trackRepository.getAll().stream().limit(1).findFirst().get();
 
         final Track track = underTest.track(expectedTrack.getId());
@@ -71,20 +69,19 @@ public class TrackApiTest {
     }
 
     @Test
-    public void shouldThrowIfTimeout() {
+    void shouldThrowIfTimeout() {
         final Throwable thrown = catchThrowable(
-                () -> underTest.track(UUID.randomUUID(), Collections.singletonMap("delay", Boolean.TRUE)));
+            () -> underTest.track(UUID.randomUUID(), Collections.singletonMap("delay", Boolean.TRUE)));
 
         assertThat(thrown)
-                .isInstanceOf(FeignException.class)
-                .hasCauseInstanceOf(SocketTimeoutException.class);
+            .isInstanceOf(FeignException.class)
+            .hasCauseInstanceOf(SocketTimeoutException.class);
     }
 
     @Test
-    public void shouldReturnNullIfNotFound() {
+    void shouldReturnNullIfNotFound() {
         final Track track = underTest.track(UUID.randomUUID());
 
         assertThat(track).isNull();
     }
-
 }
